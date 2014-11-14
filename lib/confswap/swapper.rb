071cceddcb7af6d271bd48d40c5947861b9d6921
Tom Cowling <tom.cowling@gmail.com>
2014-11-14 10:50:24 +0000

    Updates structure to use command line tool

---
+++ lib/confswap/swapper.rb
@@ -0,0 +1,7 @@
+module Confswap
+  class Swapper
+    attr_accessor :variables
+    def render_input input
+    end				
+  end
+end
---
+++ spec/confswap_spec.rb
@@ -0,0 +1,23 @@
+require 'confswap'
+
+describe 'confswap' do
+	before :each do
+    @test_swapper = Confswap::Swapper.new
+	end
+
+	it 'takes a hash of environment variables' do
+		@test_swapper.variables = sample_variables
+	end
+
+  it 'should output an empty string for an empty input regardless of variablesdefined' do
+		@test_swapper.variables = {}
+		@test_swapper.render_input ''
+
+		@test_swapper.variables = sample_variables
+		@test_swapper.render_input ''
+	end
+
+	def sample_variables
+		{ :item1 => "localhost", :item2 => "8.8.8.8" }
+	end
+end

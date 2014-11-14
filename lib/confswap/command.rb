260dfcd2bf92caca10035efe8227edb9bc43e331
Tom Cowling <tom.cowling@gmail.com>
2014-11-14 16:13:16 +0000

    Make default output_filepath

--- lib/confswap/command.rb
+++ lib/confswap/command.rb
@@ -22,14 +22,24 @@ module Confswap
 		     puts Confswap::VERSION
 	       return 0
        end
-			 Confswap::Executor
-			 info = Confswap::ConfigurationFileReader.read configuration_filepath
-			 env_variables = Confswap::EnvironmentVariableReader.read_variables
+
+			 output_filepath_default = './test.conf.out' if output_filepath.nil?
+			 
+			 if File.exists? configuration_filepath
+			   configuration_template = Confswap::ConfigurationFileReader.read configuration_filepath
+			   env_variables = Confswap::EnvironmentVariableReader.read_variables
+			   output = configuration_template % env_variables
+			   File.write(output_filepath || output_filepath_default , output)
+			   return 0
+			 else
+         puts "Error: Configuration template filepath #{configuration_filepath} does not exist"
+				 return 1
+			 end
     end
 		
-		option ['-r','--replace'], :flag, "Replace (Overwrite) the template file withe variable filled output", :attribute_name => :force
+		option ['-f','--force'], :flag, "Overwrite file if it already exists"
 		option ['-v', '--version'], :flag, "The version of confswap you are running"
-    option ['-o', '--output'], "FILE", "Writes the output from a config to a file", :attribute_name => :output_file
+    option ['-o', '--output'], "FILE", "Writes the output from a config to a file", :attribute_name => :output_filepath
 		option ['--verbose'], :flag, "Be more verbose"
 
 		parameter "TEMPLATE_FILE", "Path to the configuration file", :attribute_name => :configuration_filepath
